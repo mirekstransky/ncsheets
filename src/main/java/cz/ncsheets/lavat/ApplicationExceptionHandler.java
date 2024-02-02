b541e1a1e6ef7eb5248568421976733b3aab68d3
mirek <mirek.stransky@gmail.com>
2024-02-02 20:08:38 +0100

    Update settings and Global Exception Handler

---
+++ src/main/java/cz/ncsheets/lavat/ApplicationExceptionHandler.java
@@ -0,0 +1,90 @@
+package cz.ncsheets.lavat;
+
+import cz.ncsheets.lavat.exception.*;
+import org.springframework.http.*;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ErrorResponse error = new ErrorResponse();
+        error.setMessage("Failed to read request");
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        ErrorResponse error = new ErrorResponse();
+        error.setMessage("Validation failed for argument");
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Object> handlerException(NotFoundException ex) {
+
+        ErrorResponse error = new ErrorResponse();
+        error.setMessage(ex.getLocalizedMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DuplicateObjectException.class)
+    public ResponseEntity<Object> handlerException(DuplicateObjectException ex) {
+
+        ErrorResponse error = new ErrorResponse();
+        error.setMessage(ex.getLocalizedMessage());
+        error.setStatus(HttpStatus.CONFLICT.value());
+
+        return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+    @ExceptionHandler(ObjectValidationFailedException.class)
+    public ResponseEntity<Object> handlerException(ObjectValidationFailedException ex) {
+
+        ErrorResponse error = new ErrorResponse();
+        error.setMessage(ex.getLocalizedMessage());
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(ObjectIDisNotNull.class)
+    public ResponseEntity<Object> handlerException(ObjectIDisNotNull ex) {
+
+        ErrorResponse error = new ErrorResponse();
+        error.setMessage(ex.getLocalizedMessage());
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BadArgumentType.class)
+    public ResponseEntity<Object> handlerException(BadArgumentType ex) {
+
+        ErrorResponse error = new ErrorResponse();
+        error.setMessage(ex.getLocalizedMessage());
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Object> handlerException(Exception ex) {
+
+        ErrorResponse error = new ErrorResponse();
+        error.setMessage("Illegal input value");
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
