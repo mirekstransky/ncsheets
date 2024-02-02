56d764b1ee440f167a51175557bf50e0755b803e
mirek <mirek.stransky@gmail.com>
2024-02-02 20:02:47 +0100

    Create constants for Validate fields

---
+++ src/main/java/cz/ncsheets/lavat/constants/Constants.java
@@ -0,0 +1,13 @@
+package cz.ncsheets.lavat.constants;
+
+public class Constants {
+    public static final String NOT_BLANK  = "Subject cannot be blank";
+    public static final String NOT_EMPTY  = "Subject cannot be empty";
+    public static final String NOT_NULL  ="Subject cannot be null";
+    public static final String DECIMAL_MIN = "1.0";
+    public static final String DECIMAL_MIN_MESSAGE = "Subject must be greater than {value}";
+    public static final int SIZE_MIN = 1;
+    public static final int SIZE_MAX = 50;
+
+
+}
