102bcadd96e85ca076f018f88ae323a89e6e3ef3
mirek <mirek.stransky@gmail.com>
2024-02-03 02:12:31 +0100

    Set query findByName

--- src/main/java/cz/ncsheets/lavat/repository/HolderRepository.java
+++ src/main/java/cz/ncsheets/lavat/repository/HolderRepository.java
@@ -1,10 +1,16 @@
 package cz.ncsheets.lavat.repository;
 
+import cz.ncsheets.lavat.entity.Adapter;
 import cz.ncsheets.lavat.entity.Holder;
-import jdk.jfr.Registered;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface HolderRepository extends JpaRepository<Holder,Long> {
+    @Query("SELECT m FROM Holder m WHERE m.name = :name")
+    Optional<Adapter> findComponentByName(@Param("name") String name);
 }
--- src/main/java/cz/ncsheets/lavat/repository/HoldersizeRepository.java
+++ src/main/java/cz/ncsheets/lavat/repository/HoldersizeRepository.java
@@ -1,6 +1,6 @@
 package cz.ncsheets.lavat.repository;
 
-import cz.ncsheets.lavat.entity.Adapter;
+import cz.ncsheets.lavat.entity.Holdersize;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface AdapterRepository extends JpaRepository<Adapter,Long> {
-    @Query("SELECT m FROM Adapter m WHERE m.name = :name")
-    Optional<Adapter> findComponentByName(@Param("name") String name);
+public interface HoldersizeRepository extends JpaRepository<Holdersize,Long> {
+    @Query("SELECT m FROM Holdersize m WHERE m.name = :name")
+    Optional<Holdersize> findComponentByName(@Param("name") String name);
 }
