5aca764060c49601d47496532ae3e067ed04c887
mirek <mirek.stransky@gmail.com>
2024-02-03 22:24:37 +0100

    set new handleHttpRequestMethodNotSupported

--- src/main/java/cz/ncsheets/lavat/ApplicationExceptionHandler.java
+++ src/main/java/cz/ncsheets/lavat/ApplicationExceptionHandler.java
@@ -1,9 +1,11 @@
 package cz.ncsheets.lavat;
 
 import cz.ncsheets.lavat.exception.*;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.*;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
@@ -49,6 +51,15 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler 
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ErrorResponse error = new ErrorResponse();
+        error.setMessage(ex.getLocalizedMessage());
+        error.setStatus(HttpStatus.METHOD_NOT_ALLOWED.value());
+
+        return new ResponseEntity<>(error, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
     @ExceptionHandler(ObjectValidationFailedException.class)
     public ResponseEntity<Object> handlerException(ObjectValidationFailedException ex) {
 
@@ -56,6 +67,14 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler 
         error.setMessage("Object validation failed");
         error.setStatus(HttpStatus.CONFLICT.value());
         return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handlerException(ConstraintViolationException ex) {
+
+        ErrorResponse error = new ErrorResponse();
+        error.setMessage(ex.getLocalizedMessage());
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<Object> handlerException(NotFoundException ex) {
