61496a6e0ebe95344582ca576e8ce86780053bfc
mirek <mirek.stransky@gmail.com>
2024-02-03 22:27:34 +0100

    set new constructor for ObjectIDisNotNull - set parameter String type

--- src/main/java/cz/ncsheets/lavat/exception/ObjectIDisNotNull.java
+++ src/main/java/cz/ncsheets/lavat/exception/ObjectIDisNotNull.java
@@ -2,6 +2,10 @@ package cz.ncsheets.lavat.exception;
 
 public class ObjectIDisNotNull extends RuntimeException{
 
+    public ObjectIDisNotNull(String type)
+    {
+        super("The object "+type+" ID is not null, unable to set ID");
+    }
     public ObjectIDisNotNull()
     {
         super("The object ID is not null, unable to set ID");
