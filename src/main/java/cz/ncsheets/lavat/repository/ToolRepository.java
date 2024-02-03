165373857b2178c6a4d55c48d3ff87bc4d0f0e77
mirek <mirek.stransky@gmail.com>
2024-02-03 22:33:44 +0100

    create query findComponentByName

--- src/main/java/cz/ncsheets/lavat/repository/ToolRepository.java
+++ src/main/java/cz/ncsheets/lavat/repository/ToolRepository.java
@@ -1,9 +1,16 @@
 package cz.ncsheets.lavat.repository;
 
 import cz.ncsheets.lavat.entity.Tool;
+import cz.ncsheets.lavat.entity.Tooltype;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ToolRepository extends JpaRepository<Tool,Long> {
+    @Query("SELECT m FROM Tool m WHERE m.name = :name")
+    Optional<Tool> findComponentByName(@Param("name") String name);
 }
