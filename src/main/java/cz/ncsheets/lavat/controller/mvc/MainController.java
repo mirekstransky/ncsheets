4342736ec173a4cdc0810b32bdf4d58d24a10acc
mirek <mirek.stransky@gmail.com>
2024-02-10 21:55:59 +0100

    create query for Holder repository and holder template

--- src/main/java/cz/ncsheets/lavat/controller/mvc/MainController.java
+++ src/main/java/cz/ncsheets/lavat/controller/mvc/MainController.java
@@ -1,29 +1,58 @@
 package cz.ncsheets.lavat.controller.mvc;
 
-import cz.ncsheets.lavat.service.rest.HolderServiceREST;
+import cz.ncsheets.lavat.entity.Assemble;
+import cz.ncsheets.lavat.entity.filter.HolderForm;
+import cz.ncsheets.lavat.service.AssembleService;
+import cz.ncsheets.lavat.service.HolderService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Controller
 public class MainController {
 
-    HolderServiceREST holderServiceREST;
+    HolderService holderService;
+    AssembleService assembleService;
 
     @GetMapping("/")
     public ModelAndView index() {
+
         return new ModelAndView("mainpage");}
     @GetMapping("/components")
     public ModelAndView componentsView() {
-        return new ModelAndView("components");}
+        return new ModelAndView("components");
+    }
 
     @GetMapping("/components/holder")
-    public ModelAndView index(@PageableDefault(sort = {"name"}) Pageable pageable) {
+    public ModelAndView index(@PageableDefault(sort = {"name"},size = 8) Pageable pageable) {
         return new ModelAndView("holder")
-                .addObject("holders", holderServiceREST.getPageAll(pageable));
+                .addObject("items", holderService.getPageAll(pageable)).addObject("form",new HolderForm());
     }
+    @GetMapping("/components/holder/filter")
+    public ModelAndView filterHolder(@ModelAttribute("form") HolderForm form, @PageableDefault(sort = {"name"}) Pageable pageable) {
+        return new ModelAndView("holder")
+                .addObject("items", holderService.findPageByHolderForm(form,pageable));
+    }
+    @PostMapping("/component/holder/delete")
+    public Object deleteHolder(@RequestParam("holderId") long id,Pageable pageable){
+        Page<Assemble> assemble = assembleService.findComponentsWithAdapter(id,pageable);
+        if (assemble.getSize() != 0) {
+            return new ModelAndView("errIntegrityHolder")
+                    .addObject("items",assemble);
+        }
+        holderService.deleteComponent(id);
+
+        return "redirect:/components/holder";
+    }
+
 }
--- src/main/java/cz/ncsheets/lavat/repository/HolderRepository.java
+++ src/main/java/cz/ncsheets/lavat/repository/HolderRepository.java
@@ -1,7 +1,9 @@
 package cz.ncsheets.lavat.repository;
 
-import cz.ncsheets.lavat.entity.Adapter;
 import cz.ncsheets.lavat.entity.Holder;
+import cz.ncsheets.lavat.entity.filter.HolderForm;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,6 +16,28 @@ public interface HolderRepository extends JpaRepository<Holder,Long> {
     @Query("SELECT m FROM Holder m WHERE m.name = :name")
     Optional<Holder> findComponentByName(@Param("name") String name);
 
+    @Query("SELECT m FROM Holder m WHERE m.holderSize = :size")
+    Optional<Holder> findComponentBySize(@Param("size") Long size);
 
+//    @Query("SELECT e FROM Holder e WHERE " +
+//            "(:name IS NULL OR e.name LIKE %:name%) AND " +
+//            "(:diameter IS NULL OR e.diameter = :diameter) AND " +
+//            "(:length IS NULL OR e.length = :length) AND " +
+//            "(:size IS NULL OR e.holderSize.name LIKE %:size%)")
+//    Page<Holder> findPageByName(@Param("name") String name,
+//                                @Param("diameter") Double diameter,
+//                                @Param("length") Double length,
+//                                @Param("size") String size,
+//                                Pageable pageable);
 
+    @Query("SELECT e FROM Holder e WHERE " +
+            "(:#{#form.name} IS NULL OR e.name LIKE %:#{#form.name}%) AND " +
+            "(:#{#form.fromDiameter} IS NULL OR e.diameter >= :#{#form.fromDiameter}) AND " +
+            "(:#{#form.toDiameter} IS NULL OR e.diameter <= :#{#form.toDiameter}) AND " +
+            "(:#{#form.fromLength} IS NULL OR e.length >= :#{#form.fromLength}) AND " +
+            "(:#{#form.toLength} IS NULL OR e.length <= :#{#form.toLength}) AND " +
+            "(:#{#form.size} IS NULL OR e.holderSize.name LIKE %:#{#form.size}%)")
+    Page<Holder> findPageByHolderForm(@Param("form") HolderForm form, Pageable pageable);
 }
+
+
