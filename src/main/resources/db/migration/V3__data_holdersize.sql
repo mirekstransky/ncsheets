4e22a8e75ce148a30ba6315a97c2158e75b3b982
mirek <mirek.stransky@gmail.com>
2024-02-10 21:53:49 +0100

    update datebase validations

--- src/main/resources/db/migration/V3__data_holdersize.sql
+++ src/main/resources/db/migration/V3__data_holdersize.sql
@@ -1,9 +1,29 @@
 INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('Centro-P ER30',50,4,50);
-INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('Centro-P ER34',50,4,100);
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('PPC-P ER34',50,4,100);
 INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('Centro-P ER36',50,4,150);
-INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('Centro-P ER38',50,4,200);
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('PPC-P ER38',50,4,200);
 
-INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('Centro-P ER40',50,4,50);
-INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('Centro-P ER42',50,4,100);
-INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('Centro-P ER44',50,4,150);
-INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('Centro-P ER46',50,4,200);
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('Centro-P ER40',25,4,50);
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('HDC-P ER42'   ,30,4,100);
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('Centro-P ER44',35,4,150);
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('Centro-P ER46',40,4,200);
+
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('HDC ER48'     ,42.5,4,50);
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('Centro-P ER50',50,4,100);
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('HDC-P ER52'   ,52.5,4,150);
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('Centro-P ER54',60,4,200);
+
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('PPC ER56'     ,40,4,50);
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('Centro-P ER58',25,4,100);
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('HDC-P ER60'   ,65,4,150);
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('Centro-P ER70',75,4,200);
+
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('PPC ER72'     ,50,3,50);
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('Centro-P ER74',50,3,100);
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('HDC-P ER76'   ,95,3,150);
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('Centro-P ER78',50,3,200);
+
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('PPC ER80'     ,50,1,50);
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('Centro-P ER82',50,1,100);
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('HDC-P ER84'   ,50,1,150);
+INSERT INTO holder (name, diameter,holdersize_id,length) VALUES ('Centro-P ER86',50,1,200);
